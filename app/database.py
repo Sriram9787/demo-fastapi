6d10cc1c5bd00865d44c183727fcc7abbd7f0393
sriram <rsriram.rthoughts@gmail.com>
2021-12-29 11:48:58 +0530

    use

--- app/database.py
+++ app/database.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import sessionmaker
 from .config import settings
 
 
-SQL_DATABASE_URL = "postgres://lenormoxtlobjq:3131d0dc2a85a9295585e5c4db4fc35cdc3d1cb3819b798d795ce9043fd8e3bc@ec2-44-198-211-34.compute-1.amazonaws.com:5432/d7hviodhvs8eg9"
+SQL_DATABASE_URL = settings.database_url
 
 ENGINE = create_engine(SQL_DATABASE_URL)
 
